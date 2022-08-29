00e1ea3e65076d08a901c9b841b38827e49f3948
Bohdan Budzyk <webhawky@MacBook-Pro-Bohdan.local>
2022-08-29 16:57:35 +0300

    done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -33,6 +33,8 @@ export default function Form({ onSubmit }) {
   const reset = () => {
     const setName = '';
     const setNumber = '';
+    console.log(setName);
+    console.log(setNumber);
   };
   const id = { nanoid };
 
